98e7a022a1f4513e8d20b9e4066cf89662623f20
NatashaLokiJung <427370@rts-365.dk>
2020-11-16 14:13:59 +0100

    Små justeringer, overvejer et nyt projekt

--- src/conponents/Nav.js
+++ src/conponents/Nav.js
@@ -6,8 +6,6 @@ import SelectClass from "./SelectClass";
 import navImg from "../img/header.png";
 
 const Nav = (props) => {
-    // const accountName = props.accountName;
-
     const styleNav = css`
         display: flex;
         align-items: center;
@@ -32,7 +30,7 @@ const Nav = (props) => {
         <div css={styleNav}>
             <img src={navImg} alt="navigation header"></img>
             <div css={navContent}>
-                <SelectClass />
+                {/* <SelectClass /> */}
                 <SearchAccount />
             </div>
         </div>
